e1c2506c13d3bac447d7cb17d22b02ecdf86e418
Devv1507 <davide.valencia.v@gmail.com>
2025-11-17 02:03:02 -0500

    fix: renamed of file

--- src/users/infrastructure/serializers.py
+++ src/users/infrastructure/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from src.users.infrastructure.models import User, Role
+from src.users.models import User, Role
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
