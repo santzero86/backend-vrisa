2ce64421695611101723f7327fbc9aae308b1269
Devv1507 <davide.valencia.v@gmail.com>
2025-11-23 10:47:36 -0500

    feat: added new routes for login and token refresh

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from src.users.views import UserRegistrationView, UserDetailView
+from rest_framework_simplejwt.views import TokenRefreshView
+from src.users.views import CustomTokenObtainPairView, UserDetailView, UserRegistrationView
 
 urlpatterns = [
+    # Autenticación (JWT)
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Gestión de Usuarios
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
 ]
