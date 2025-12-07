48fdde52695efef031d6032b7e552c973fc28f37
Devv1507 <davide.valencia.v@gmail.com>
2025-12-07 18:49:57 -0500

    feat: added new RUN_MIGRATIONS variable to avoid over-lapping of migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Aplicando migraciones de base de datos..."
-python manage.py migrate
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+    echo "Aplicando migraciones de base de datos..."
+    python manage.py migrate
 
-echo "Poblando la base de datos con datos semilla..."
-python manage.py seed_db
+    echo "Poblando la base de datos con datos semilla..."
+    python manage.py seed_db
+else
+    echo "Saltando migraciones y seeds (Modo Worker/Simulador)..."
+fi
 
 exec "$@"
