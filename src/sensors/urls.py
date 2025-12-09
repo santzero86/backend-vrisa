b830e19ffa3681725e2ff9017f69b26ba5099753
Devv1507 <davide.valencia.v@gmail.com>
2025-12-09 04:28:14 -0500

    feat: added maintenance route to URLs

--- src/sensors/urls.py
+++ src/sensors/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import SensorViewSet
+from .views import MaintenanceLogViewSet, SensorViewSet
 
 router = DefaultRouter()
-router.register(r'', SensorViewSet, basename='sensor')
+router.register(r'devices', SensorViewSet, basename='sensor')
+router.register(r'maintenance', MaintenanceLogViewSet, basename='maintenance')
 
 urlpatterns = [
     path('', include(router.urls)),
